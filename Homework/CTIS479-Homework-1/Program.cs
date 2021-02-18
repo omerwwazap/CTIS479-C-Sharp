de972d6f600c618802c17a76d521f32d1d17a09d
Levent Durdalı <levent07@live.com>
2021-02-18 13:18:52 +0300

    Initial Commit

---
+++ Homework/CTIS479-Homework-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CTIS479_Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
