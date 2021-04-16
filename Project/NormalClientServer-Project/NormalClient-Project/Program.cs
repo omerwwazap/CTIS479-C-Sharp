8945cb5acea0e65564f045226d7979f966e423f1
Levent Durdalı <levent07@live.com>
2021-04-16 22:29:12 +0300

    Basic Directional Client/Server Program

---
+++ Project/NormalClientServer-Project/NormalClient-Project/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NormalClient_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ipAdd = string.Empty;
+            TcpClient client = new TcpClient();
+            try
+            {
+                Console.Write("Enter Your Ip address or leave blank for 127.0.0.1 (Port is  8000): ");
+                ipAdd = Console.ReadLine();
+
+                if (ipAdd == "")
+                {
+                    Console.WriteLine("Choosen Defult IP addres");
+                    client = new TcpClient("127.0.0.1", 8080);
+                }
+                else
+                {
+                    Console.WriteLine("Choosen IP addres" + ipAdd);
+                    client = new TcpClient(ipAdd, 8080);
+
+                }
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+
+                string s = string.Empty;
+                while (!s.Equals("Exit") || !s.Equals("exit"))
+                {
+                    Console.Write("Enter something to send to the server: ");
+                    s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+                    string server_string = reader.ReadLine();
+                    Console.WriteLine(server_string);
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Not an IP address closing the server... :(");
+                //Console.WriteLine(e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Not an IP address closing the server... :(");
+                //Console.WriteLine(e);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Client Connection has been interrupted, closing the server");
+                //Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Project/NormalClientServer-Project/NormalServer-Project/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NormalServer_Project
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TcpListener listener = null;
+            string ipAdd = string.Empty;
+            try
+            {
+                Console.Write("Enter Your Ip address or leave blank for 127.0.0.1 (Port is  8000): ");
+                ipAdd = Console.ReadLine();
+
+                if (ipAdd == "")
+                {
+                    Console.WriteLine("Choosen Defult IP addres");
+                    listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+                    listener.Start();
+                }
+                else
+                {
+                    Console.WriteLine("Choosen IP addres" + ipAdd);
+                    listener = new TcpListener(IPAddress.Parse(ipAdd), 8080);
+                    listener.Start();
+                }
+
+                Console.WriteLine("EchoServer started...");
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client connections...");
+
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    Console.WriteLine("Accepted new client connection...");
+
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    StreamWriter writer = new StreamWriter(client.GetStream());
+
+                    string s = string.Empty;
+                    while (!(s = reader.ReadLine()).Equals("Exit") || s.Equals("exit") || (s == null))
+                    {
+                        Console.WriteLine("From client -> " + s);
+                        writer.WriteLine("From server -> " + s);
+                        writer.Flush();
+                    }
+                    reader.Close();
+                    writer.Close();
+                    client.Close();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Not an IP address closing the server... :(");
+                //Console.WriteLine(e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Not an IP address closing the server... :(");
+                //Console.WriteLine(e);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Client Connection has been interrupted, closing the server");
+                //Console.WriteLine(e);
+            }
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+    }
+}
