38ca3dadb026ac911332472a8d0c866dbdbc284d
Levent Durdalı <levent07@live.com>
2021-04-16 23:14:17 +0300

    Basic Multi Threaded Client/Server

---
+++ Project/MultiThreadedClientServer-Project/MultiTClient-Project/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MultiTClient_Project
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IPAddress ip_address = IPAddress.Parse("127.0.0.1"); //Default
+            int port = 8080; //Default
+
+            string ipAdd = string.Empty;
+            try
+            {
+                Console.Write("Enter Your IP address or leave blank for 127.0.0.1 (Port is  8000): ");
+                ipAdd = Console.ReadLine();
+
+                if (ipAdd == "")
+                {
+                    Console.WriteLine("Invalid IP address entered. Using default IP :]");
+                }
+                else
+                {
+                    ip_address = IPAddress.Parse(ipAdd);
+                }
+
+
+            }//try
+            catch (FormatException)
+            {
+                //Console.WriteLine("Invalid IP address entered. Using default IP of: " + ip_address.ToString());
+            }
+            try
+            {
+                //Console.WriteLine("Attempting to connect to server at IP address: {0} port: {1}", ip_address.ToString(), port);
+
+                TcpClient client = new TcpClient(ip_address.ToString(), port);
+
+                Console.WriteLine("Connection successful :]");
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+
+                string s = String.Empty;
+                while (!s.Equals("Exit") || !s.Equals("exit"))
+                {
+                    Console.Write("Enter something to send: ");
+                    s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+                    if (!s.Equals("Exit") || !s.Equals("exit"))
+                    {
+                        String server_string = reader.ReadLine();
+                        Console.WriteLine(server_string);
+                    }
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }//rty
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }//Main
+    }
+}
---
+++ Project/MultiThreadedClientServer-Project/MultiTServer-Project/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MultiTServer_Project
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TcpListener listener = null;
+            try
+            {
+                //Unlike our Single Threaded Server IPAddress.Any will listen to all Netwotk Interfaces and their Ip address
+                //Ex. Wifi Module is 192.168.1.2 and Ethernet Interface is 172.0.0.1 
+                //IPAddress.Any will listen to both
+                listener = new TcpListener(IPAddress.Any, 8080);
+                listener.Start();
+                Console.WriteLine("Multi Threaded Server Has Started... :]");
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client connections... :]");
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    Console.WriteLine("Whoa a new client connection :]");
+
+                    //Start a new Thread for every new client connection
+                    Thread t = new Thread(ProcessClientRequests);
+
+                    t.Start(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }//Main()
+
+        private static void ProcessClientRequests(object argument)
+        {
+            //Same Code Taken From Single Threaded Server Program
+            TcpClient client = (TcpClient)argument;
+            try
+            {
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                string s = String.Empty;
+                while (!(s = reader.ReadLine()).Equals("Exit") || s.Equals("exit") || (s == null))
+                {
+                    Console.WriteLine("From client -> " + s);
+                    writer.WriteLine("From server -> " + s);
+                    Console.WriteLine("Client connected with IP {0}", client.Client.LocalEndPoint); //Show Connected IP
+                    writer.Flush();
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+                Console.WriteLine("Client connection terminated :[");
+            }//rty
+            // Have only one execption unlike the Single Threaded Server Program becasue we dont have any IP inputs here
+            catch (IOException)
+            {
+                Console.WriteLine("Problem with client communication :[");
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }//ProcessClientRequests()
+    }
+}
