6c255c27b024493b4a8568cff2b5c1bb7ed6e174
Levent Durdalı <levent07@live.com>
2021-04-22 19:31:32 +0300

    more sad

--- Homework/LeventDurdali-HW2/Controllers/HomeController.cs
+++ Homework/LeventDurdali-HW2/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace LeventDurdali_HW2.Controllers
             return View();
         }
 
+        public IActionResult Drone()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
