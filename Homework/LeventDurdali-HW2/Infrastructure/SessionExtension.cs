1eb823b51d89c85abeecff0f2cb476421e57170a
Levent Durdalı <levent07@live.com>
2021-04-23 18:13:34 +0300

    sad

---
+++ Homework/LeventDurdali-HW2/Infrastructure/SessionExtension.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LeventDurdali_HW2.Infrastructure
+{
+    public static class SessionExtention
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.Set(key, JsonSerializer.Serialize(value));
+        }
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.Get(key);
+
+            return value == null ? default(T) :
+                JsonSerializer.Deserialize<T>(value);
+        }
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+            return sessionData == null
+            ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
+        }
+    }
+}
+
